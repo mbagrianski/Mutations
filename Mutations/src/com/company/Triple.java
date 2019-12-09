bfab8d2a80ee9228b93cae1b29152487b5264040
mbagrianski <46272573+mbagrianski@users.noreply.github.com>
2019-12-09 08:49:27 -0500

    Update Triple.java

--- Mutations/src/com/company/Triple.java
+++ Mutations/src/com/company/Triple.java
@@ -1,17 +1,5 @@
 package com.company;
 
-/*
-Name: Ethan Zhang
-Course: ICS4U1-01
-Teacher: Mr. Radulovic
-Date Finished: December 8, 2019
-Assignment: ADT-Gene Assignment
-Description:
-This class is used to store genes. The three essential information about genes are:
-1) Gene string, which shows the actual gene
-2) The probability required to reach this gene
-3) The number of steps taken to reach this gene
-*/
 public class Triple{
     //After making the variables public, we can directly change the values instead of writing getters/setters.
     public String x;
